0b919e23aa20dd45511de8c080f075810bdcf6c5
SneyderRodriguez <sneyder2112_@hotmail.com>
2026-07-06 18:43:17 -0500

    Tajer Poke Api

---
+++ explorer.js
@@ -0,0 +1,24 @@
+// const prompt = require("prompt-sync")();
+
+async function pokedex() {
+  const respuesta = await fetch("https://pokeapi.co/api/v2/pokemon/snorlax");
+  // console.log(respuesta);
+
+   const datos = await respuesta.json();
+   //console.log(datos.name, datos.id , datos.height, datos.weight, datos.types, datos.stats, datos.abilities);
+
+   for(i=0; i<datos.types.length; i++){
+    console.log(datos.types[i].type.name);
+   }
+
+   for(j=0; j<datos.stats.length; j++){
+    console.log(datos.stats[j].stat.name);
+    console.log(datos.stats[j].base_stat);
+   }
+
+   for(i=0; i<datos.abilities.length; i++){
+    console.log(datos.abilities[i].ability.name);
+   }
+}
+
+pokedex();
